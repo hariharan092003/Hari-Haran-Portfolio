98d2e0df78ebea3304324cc26995b249fa3196df
hariharan092003 <110252469+hariharan092003@users.noreply.github.com>
2024-03-25 14:43:40 +0530

    added the files

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    const menu=document.querySelector(".menu-lin");
+    const  icon=document.querySelector(".ham-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+}
